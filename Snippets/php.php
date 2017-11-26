5e84074a07671f543855ddc89086cf513a3f4134
Zinovij Lipinski <zinovi@egamings.com>
2017-11-26 13:21:26 +0200

    app3k components

--- Snippets/php.php
+++ Snippets/php.php
@@ -11,5 +11,25 @@
 	$DB_connection_id = mysql_connect($mysql_db_host, $mysql_db_login, $mysql_db_password) or die(mysql_error());
 	$DB_select_db = mysql_select_db($mysql_db_databasename) or die(mysql_error());
 	$DB_charset = mysql_query("SET NAMES 'utf8'");
-  
+
+
+
+
+
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+}
+function endsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+
+    return $length === 0 || 
+    (substr($haystack, -$length) === $needle);
+}
+
+
+
+
 ?>
