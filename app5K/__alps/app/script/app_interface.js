89a3f409ba77aa40fcb7da6d178915682d06236b
Zinovij Lipinski <zinovi@egamings.com>
2018-08-28 13:43:49 +0300

    rescroll

--- app5K/__alps/app/script/app_interface.js
+++ app5K/__alps/app/script/app_interface.js
@@ -8,12 +8,18 @@
     Sample:
         app.do({
             name:       'UniquePointer',
-            when:       '.myElement--trigger',
+            when:       '.myElement__Trigger',
             has:        'click',
-            hasOptions: {},
-            do:         'addClass',
+            removeClass:'cssClassName',
+            for:        '.myElement__targetToChange'
+        });
+        app.do({
+            name:       'UniquePointer',
+            when:       '.myElement__Trigger',
+            has:        'scrollTop',
+            hasOptions: { offset:'-21%' },
             addClass:   'cssClassName',
-            for:        '.myElement--target-to-change'
+            for:        '.myElement__targetToChange'
         });
 */
 function zlAppMaster() {
@@ -23,32 +29,48 @@ function zlAppMaster() {
         scrollTop: 'scroll'
     };
     this.hasSilentReports = false;
-    // PUBLIC
+    // PUB
     this.do = function(dna) {
         this.InsertIntoNucleus(dna);
         return this;
     }
     this.Run = function() {
         this.report('start', 'AppMaster.Run');
-        this.Delegate.Prepare(this);
+        this.Delegator.Prepare(this);
         for ( var i in this.nucleus ) this.Ribosome(this.nucleus[i]);
         this
             .report('divider')
-            .report('All actions are updated. Type for details:')
+            .report('Interface update completed. Console command:')
             .report('app.proteins.ACTION_NAME')
             .report('divider')
             .report('end');
         return this;
     }
-    // PRIVATE
+    // HQ
     this.nucleus = [];
     this.proteins = {};
-    this.InsertIntoNucleus = function(dna) {
-        if (!dna) return false;
-        if (!dna.name) dna.name = 'Ogodei_'+this.nucleus.length;
-        this.nucleus.push(dna);
+    this.Ribosome = function(dna) {
+        if (!dna || !dna.name) return false;
+        var name = dna.name;
+        var protein = { name:name, dna:{} };
+        _.assign( protein, this.RibosomeGetsEvent(dna) );
+        _.assign( protein, this.RibosomeGetsElements(dna) );
+        _.assign( protein, this.RibosomeGetsAction(dna) );
+        _.assign( protein.dna, dna );
+        this.proteins[name] = protein;
+        if ( protein.event_type && this.Delegator[protein.event_type] ) {
+            this.Delegator[protein.event_type](this.proteins[name]);
+            this.report('Interface was updated for action: '+name);
+        }
         return true;
     }
+    this.Delegator = {
+        Prepare: function(app) {
+            app.Delegator.click = app.DelegateClick;
+            app.Delegator.scroll = app.DelegateScroll;
+        }
+    }
+    // INTERFACE
     this.report =function (msg,data) {
         if ( this.hasSilentReports ) return this;
         if (msg==='start') console.group(data);
@@ -62,16 +84,10 @@ function zlAppMaster() {
         }
         return this;
     }
-    this.Ribosome = function(dna) {
-        if (!dna || !dna.name) return false;
-        var name = dna.name;
-        var protein = { name:name, dna:{} };
-        _.assign( protein, this.RibosomeGetsEvent(dna) );
-        _.assign( protein, this.RibosomeGetsElements(dna) );
-        _.assign( protein, this.RibosomeGetsAction(dna) );
-        _.assign( protein.dna, dna );
-        this.proteins[name] = protein;
-        this.DoRibosomeTranslation(name, app);
+    this.InsertIntoNucleus = function(dna) {
+        if (!dna) return false;
+        if (!dna.name) dna.name = 'Ogodei_'+this.nucleus.length;
+        this.nucleus.push(dna);
         return true;
     }
     this.RibosomeGetsEvent = function(dna) {
@@ -97,49 +113,36 @@ function zlAppMaster() {
     }
     this.RibosomeGetsAction = function(dna) {
         var action = dna.do ? dna.do : false;
+        var action_operand;
         if (!action) {
             if (dna.removeClass) action = 'removeClass';
             if (dna.toggleClass) action = 'toggleClass';
             if (dna.addClass) action = 'addClass';
             if (dna.func) action = 'func';
         }
+        if ( action && action !== 'func') action_operand = dna[action];
         var func = dna.func ? dna.func : false;
         return {
             action: action,
+            action_operand: action_operand,
             func: func
         };
     }
-    this.DoRibosomeTranslation = function(name, app) {
-        var protein = app.proteins[name];
-        if ( !protein && !protein.event_type ) return false;
-        this.Delegate[protein.event_type](app.proteins[name]);
-        this.report('Interface was updated for action: '+name);
-        return true;
-    }
     this.CreateClickFunction = function(protein) {
-        if ( !protein.func ) switch ( protein.action ) {
-            case 'removeClass':
-                protein.func = function(e){
-                    protein.tel.classList.remove(protein.dna.removeClass);
-                    return true;
-                }
-                break;
-            case 'addClass':
-                protein.func = function(e){
-                    protein.tel.classList.add(protein.dna.addClass);
-                    return true;
-                }
-                break;
-        }
+        if ( protein.func ) return protein.func;
+        var supportedActions = [
+                'addClass',
+                'removeClass',
+                'toggleClass'
+        ];
+        if ( protein.action && supportedActions.indexOf(protein.action) > -1)
+            protein.func = function(e){
+                $(protein.tel)[protein.action](protein.dna[protein.action]);
+                return true;
+            }
         return protein.func;
     }
     // EXPORT
-    this.Delegate = {
-        Prepare: function(app) {
-            app.Delegate.click = app.DelegateClick;
-            app.Delegate.scroll = app.DelegateScroll;
-        }
-    }
     this.DelegateClick = function(protein) {
         app.CreateClickFunction(protein);
         return zlClick.do(protein);
@@ -148,9 +151,15 @@ function zlAppMaster() {
         var scroll_dna = {
             name: protein.name,
             el: protein.el,
-            tel: protein.tel
+            tel: protein.tel,
+            event_type: protein.event_type,
+            event_name: protein.event_name,
+            hasOptions: protein.dna.hasOptions,
+            action: protein.action,
+            symbol: protein.action_operand,
+            func: protein.func
         };
-        if ( protein.hasOptions ) _.assign(scroll_dna,protein.hasOptions);
+        if ( protein.dna.hasOptions ) _.assign(scroll_dna,protein.dna.hasOptions);
         switch ( protein.action ) {
             case 'addClass':
                 scroll_dna.symbol = protein.dna.addClass;
@@ -172,6 +181,10 @@ var app = new zlAppMaster();
 */
 function zlClickMaster() {
     this.do = function(dna) {
+        this.ActivateEvent(dna);
+        return true;
+    }
+    this.ActivateEvent = function(dna) {
         if ( !dna.el || !dna.func ) return false;
         dna.el.addEventListener('click', dna.func);
         return true;
@@ -195,75 +208,82 @@ var zlClick = new zlClickMaster();
         });
 */
 function zlScrollMaster() {
+    // PUB
+    this.AddAction = function(dna) {
+        this.nucleus[dna.name] = {};
+        _.assign(this.nucleus[dna.name], dna);
+        return this;
+    }
     this.Run = function(){
         this.DoAllActions();
+        return this;
+    }
+    // HQ
+    this.nucleus = {}; // stored dna for future actions
+    this.actions = {}; // active actions
+    this.Ribosome = function(dna) {
+        var name = dna.name;
+        this.actions[name] = { name:name, dna:dna };
+        _.assign(this.actions[name], this.RibosomeGetsElements(dna));
+        if (!this.actions[name].el)
+            return this.report('Warning, there are no el!', dna);
+        _.assign(this.actions[name], {
+            symbol:dna.symbol,
+            direction:dna.direction
+        });
+        _.assign(this.actions[name], this.RibosomeGetsFunction(dna));
+        this.DelegateAction(this.actions[name]);
+        return this;
+    }
+    // INTERFACE
+    this.report = function(msg,data) {
+        console.log('zlScrollMaster: '+msg);
+        console.log(data);
+        return this
     }
     this.DoAllActions = function() {
         for ( var name in this.nucleus )
-            this.DoAction(this.nucleus[name]);
+            this.Ribosome(this.nucleus[name]);
         return this;
     }
-    this.DoAction = function(dna) {
-        var order = this.ribosome(dna);
-        if (!order.el) return this.report('Warning, there are no el!', dna);
-        var func = order.func;
-        if (!order.func) order.func = function(direction) {
-            var o = zlScroll.actions[order.name];
+    this.RibosomeGetsElements = function(dna) {
+        var el_trigger, el_target;
+        if (dna.el) el_trigger = dna.el;
+            else if (dna.when) el_trigger = document.querySelector(dna.when);
+        if (dna.tel) el_target = dna.tel;
+            else if (dna.for)  el_target = document.querySelector(dna.for);
+            else el_target = el_trigger;
+        return {
+            el:  el_trigger,
+            tel: el_target
+        }
+    }
+    this.RibosomeGetsFunction = function(dna) {
+        var func = dna.func;
+        if (!func) func = function(direction) {
+            var o = zlScroll.actions[dna.name];
             if ( o.direction && o.direction !== direction ) {
                  o.tel.classList.remove(o.symbol);
             } else {
                 o.tel.classList.add(o.symbol);
             }
         }
-        var wp = _.assign( this.getwp(dna), {
-            element: order.el,
-            handler: order.func
-        });
-        this.actions[order.name].Waypoint = new Waypoint(wp);
-        this.actions[order.name].status = "ready";
-        return this;
+        return { func:func };
     }
-    this.AddAction = function(dna) {
-        this.nucleus[dna.name] = {};
-        _.assign(this.nucleus[dna.name], dna);
+    // EXPORT
+    this.DelegateAction = function(action) {
+        var wp = {
+            element: action.el,
+            handler: action.func,
+            offset: action.dna.offset
+        };
+        this.actions[action.name].Waypoint = new Waypoint(wp);
+        this.actions[action.name].status = "ready";
         return this;
     }
 
-    // ----------------------------------- Private
 
-    this.nucleus = {}; // stored dna for future actions
-    this.actions = {}; // active actions
-    this.report = function(msg,data) {
-        console.log('zlScrollMaster:'+msg);
-        console.log(data);
-        return this
-    }
-    this.getwp = function(params) {
-        // integration with Waypoint
-        return {
-            offset: params && params.offset ? params.offset : 0
-        }
-    }
-    this.ribosome = function(dna) {
-        // ribosome creates new action without activation
-        var name = dna.name;
-        var el;
-        if (dna.el) el=dna.el;
-        else if (dna.trigga) el=document.querySelector(dna.trigga);
-        var tel;
-        if (dna.tel) tel=dna.tel;
-        else if (dna.target) tel=document.querySelector(dna.target);
-        else tel = el;
-        this.actions[name] = {
-            name:name,
-            el:el,
-            tel:tel,
-            symbol:dna.symbol,
-            direction:dna.direction,
-            func: dna.func ? dna.func : false
-        }
-        return this.actions[name];
-    }
+
 }
 var zlScroll = new zlScrollMaster();
 /* ============================================================== */
