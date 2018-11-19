f9cf53ddad4120885439b440c2233a8a39a840c0
Zinovij Lipinski <zinovi@egamings.com>
2018-11-19 17:24:27 +0200

    DNA world

---
+++ appDev/app6K/app-story.md
@@ -0,0 +1,38 @@
+# 2018.11.18
+
+```js
+    var app = { name:'app', is:{}, do:()=>true };
+
+    //-/// noble blastula ʕ⊙ᴥ⊙ʔ
+    for ( nucleod in dna ) this[nucleod] = dna[nucleod];
+    this.name = 'status';
+    this.is   =  this.is   || {};
+    this.do   =  this.do   || function(dna){return this};
+    //-///
+```
+
+# 2018.11.19
+
+С появлением простейшей модели DNA,
+РНК мир сменился ДНК/РНК миром.
+Это упростило благородную бластулу.
+
+```js
+         // ʕ⊙ᴥ⊙ʔ
+       dna = dna||DNA;
+      for(favor in dna)
+  this[favor] = dna[favor];
+```
+
+Появилась FNA - базовая переработка РНК в отрицание.
+
+
+```js
+var FNA = (rna)=>false;
+```
+
+## неорганические block commenters
+
+//*///
+{*///}
+
--- appDev/app6K/app/script/app.js
+++ appDev/app6K/app/script/app.js
@@ -1,4 +1,4 @@
-// -------------------- Description app6001
+// ------------------------ Description app6001
 /*
     Noble objects
     - are objects
@@ -8,104 +8,166 @@
     - code blastula ʕ⊙ᴥ⊙ʔ
 
     List of noble objects
-    - king, `window.king`
-    - lord, delegator as `king.lord()`
+    - king
+    - lord
     - queen
+    - MODEL
 
-    Others
-    - block commenter iioiii
+*/ // last review: 2018.11.19
+
+/*
+    History
+    
+
+
 */
 
 
-// -------------------- Initialization
+// ------------------------ Initialization
 
 
-//*/// app blastula avant garde ʕ⊙ᴥ⊙ʔ
-var app = { name:'app', is:{} };
-app.do = function APIcommander (dna) {
-    this.nucleus = this.nucleus || [] ;
-    return dna !== false ? this.nucleus.push(dna) : false
-};
-//*///
+// ʕ⊙ᴥ⊙ʔ Noble Blastula - app avant garde
+var FNA = (rna) => false;
+var DNA = { name:'noble', is:{}, do:(rna)=>false };
+var app = DNA;
+app.do  = function StoreRequest (rna) {
+    this.nucleus = this.nucleus || [];
+    if (rna) this.nucleus.push (rna);
+    return this
+};//end
 
 
-//*/// app coronation
-app = new appKing(app);
-app // setup // v2018.11.18
-   .Delegator( new lordReport() )
-   .Delegator( new lordStatus() )
-   .Delegator( new lordMutate() )
-   .Welcome()
-; //*/// end setup
+// ʕ⊙ᴥ⊙ʔ Noble Blastula - app launcher
+app = new AppKing (app); // coronation
+app//setup              //  2018.11.19
+    .Delegator( new lordReport() )
+    .Delegator( new lordStatus() )
+    .Delegator( new lordMutate() )
+    .Welcome()
+;// .End(setup)
+
+
+app//demo
+    .do(77)
+    .report('ʕ⊙ᴥ⊙ʔ Welcome King')
+    .report()
+;// .end
 
 
 
 
-// -------------------- Core functions
 
-function appKing(dna) {
+// ------------------------ Core functions
 
-    //*/// noble blastula ʕ⊙ᴥ⊙ʔ
-    for ( nucleod in dna ) this[nucleod] = dna[nucleod];
-    this.name =  this.name || 'yourmaster';
-    this.is   =  this.is   || {};
-    this.do   =  this.do   || function(dna){return this};
-    //*///
 
-    //*/// I'm the King
-    this.is.king = false; // not yet
-    this.description = "King of noble objects";
-    this.status = function (dna) {return true};
-    this.report = function (msg) {return this};
-    this.mutate = function (dna) {return dna};
-    this.lords  = {};
-    //*///
+function AppKing (dna) {     // ʕ⊙ᴥ⊙ʔ
+                           dna = dna||DNA;
+                          for(favor in dna)
+                      this[favor] = dna[favor];
 
-    //*///
+//..tion Description (codex)
+
+    this.name         = this.name;
+    this.about        ='King of noble objects and functions';
+    this.is.king      = false; // not yet
+    this.el           = FNA;
+    this.report       = FNA;
+    this.nucleus      = dna.nucleus || [];
+    this.team         = {};
+
+//..tion PUB API
+
+    this.Delegator = function DelegateTo (lord) {
+        lord.king            = this;
+        lord.is.overlord     = lord.king[lord.name] && true || false;
+        this.team[lord.name] = lord;
+        if (lord.is.overlord) this[lord.name] = this.team[lord.name].do;
+        return this;
+    }
     this.Welcome = function Init () {
-        this.SilentMeeting();
+        app.WelcomeTeam();
+        app.Ribosome();
         return this;
     }
-    //*///
 
-//# service workers
+//..tion Service workers
 
-    // lords services
-
-    this.Delegator = function AssignDelegators (lord) {
-        lord.king = this;
-        this.lords[lord.name] = lord;
+    this.WelcomeTeam = function ActivationProcedure (rule) {
+        for(let anyone in this.team) {
+            let lord = this.team[anyone];
+            if (lord.is.lord) this.WelcomeLord(lord)
+        }
         return this;
     }
+    this.WelcomeLord = function ActivationProcedure (lord) {
+        this[lord.name] = FNA;
+        this.Delegator(lord).report('- app.'+lord.name+'()');
+        return this;
+    }
+    this.Ribosome = function ReadRequest (rule) {
+        this.king.report('-x- some ribosome');
+        return this;
+    }
+
+//..tion Pending
 
     this.SilentMeeting = function ActivateDelegators (rules) {
         const meet = this.lords;
         let   count_them = 0;
-        for ( let name in meet ) if (meet[name].is.lord) {
+        for (let name in meet) if (meet[name].is.lord) {
             this[name] = meet[name].do;
             meet[name].is.active = true;
             count_them++;
         }
-        //*/// begin reporting
+        // reporting
         this.king.report('ʕ⊙ᴥ⊙ʔ '+count_them+' app delegators');
         for ( let name in meet ) if (meet[name].is.active) {
             this.king.report('- app.'+name+'()');
         }
-        //*///
     }
-    
+    this.status       = (rna)=>false;
 
-//# summary
+//..tion Summary (codex)
 
-    //*/// --- ʕ⊙ᴥ⊙ʔ
-    this.do      = this.do; // APIcommander
-    this.king    = this;
-    this.is.king = true;
-    //*///
+    this.is.king      = true;
+    this.do           = this.do; // belive in avant garde
+    this.king         = this;
+
+//..tion end
 }
 
 
 // lords
+
+function lordTemplate (dna) {    // ʕ⊙ᴥ⊙ʔ
+                               dna = dna||DNA;
+                              for(favor in dna)
+                          this[favor] = dna[favor];
+
+//..tion Description (codex)
+
+    this.name         ='xoxox_template';
+    this.about        ='xoxox_description';
+    this.is.lord      = false; // not yet
+
+//..tion Service workers
+
+    //xoxox
+
+//..tion Pending
+
+    //xoxox
+
+//..tion Summary (codex)
+
+    this.is.lord      = true;
+    this.do           = this.do;
+    this.king         = this.king || this;
+
+//..tion end
+}
+
+
 
 
 function lordStatus(dna) {
@@ -253,31 +315,6 @@ function lordMutate(dna) {
 }
 
 
-function lordTemplate(dna) {
-    //*/ noble blastula ʕ⊙ᴥ⊙ʔ
-    for ( nucleod in dna ) this[nucleod] = dna[nucleod];
-    this.name = 'xoxox_name';
-    this.is   =  this.is   || {};
-    this.do   =  this.do   || function(dna){return this};
-    //*/
-
-    //*/// I'm the lord
-    this.is.lord = false; // not yet
-    this.description = "xoxox_explain_meaning";
-    //*///
-
-//# service workers
-
-//# summary
-
-    //*/// --- ʕ⊙ᴥ⊙ʔ
-    this.do      = this.do;
-    this.king    = this;
-    this.is.lord = true;
-    //*///
-}
-
-
 //*/// ======================================================
 // pending
 
@@ -295,7 +332,7 @@ testing.checklist.push(
 //*///
 
 //*/// t
-app.do(3);app.do('bs');app.do(false);
+//app.do(3);app.do('bs');app.do(false);
 testing.checklist.push(
     app.nucleus.join('') === '3bs' || 'broken nucleus'
 );
