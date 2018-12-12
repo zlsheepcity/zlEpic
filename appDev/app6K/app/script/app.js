06cc433103e505bb3aa2f047a4c72a6323c33a29
Zinovij Lipinski <zinovi@egamings.com>
2018-12-12 15:42:36 +0200

    in love

--- appDev/app6K/app/script/app.js
+++ appDev/app6K/app/script/app.js
@@ -1,53 +1,73 @@
-const DNA = { name:'wild', do:(protein)=>false };
-var app = new AppKing({name:'app'});
-var q = {name:'mutate'};
-app.take(q);
-app.do(1).do(2).do(3);
-app.Welcome();
-app.do(5).do(6).do(7);
+const DNA = { name:'wild', do:(that)=>false, in:{state:false} };
+var app = new AppKing('app');
+
+app.do('report',{msg:'it’s ok'});
+
+//app.take( new QueenReporter() );
 
 
-app.do(1).do(2).do(3);
-app.take(q).love.mutate.do(4);
-var app2 = new AppKing({name:'app2'});
-app2.Welcome();
+function AppKing (alexander, dna) {
 
+           // ʕ⊙ᴥ⊙ʔ
+         dna = dna||DNA;
+        for(favor in dna)
+    this[favor] = dna[favor];
 
-app.take( new QueenMutate() );
-app.do({when:'trigger',has:'click',do:'mutate',addClass:'nigger'});
-
-
-function AppKing (dna) {
-
-    this.do   = lordKeepInMind;
-    this.doit = lordDelegator;
+    this.name = alexander || this.name;
     this.take = lordTakeTheQueen;
+    this.love = lordDelegator;
+    this.keep = lordKeepInMind;
+    this.do   = this.keep;
 
-    this.Welcome = lordWelcome;
+    this.Welcome = lordWelcomeKing;
+
+}
+function AppQueen (roxana, dna) {
 
            // ʕ⊙ᴥ⊙ʔ
-         dna = dna||{};
+         dna = dna||DNA;
         for(favor in dna)
     this[favor] = dna[favor];
 
-}
-function AppQueen (dna) {
-
-    this.do = lordKeepInMind;
-
-           // ʕ⊙ᴥ⊙ʔ
-         dna = dna||{};
-        for(favor in dna)
-    this[favor] = dna[favor];
+    this.name = roxana || this.name;
+    this.love = lordNameOfKing;
+    //this.ff    = dna || (that)=>false;
 
 }
-function lordKeepInMind (that) {
-    // store data in array
+function lordKeepInMind (that, memory) {
 
-    this.mind = [].concat( this.mind||[], [that] );
+    let  keep = this.in.theNameOf || this;
+    keep.in.mind = [].concat(
+        keep.in.mind ||[],
+        [{ do:that, with:memory }]
+    );
+
+    return this;
+}
+function lordTakeTheQueen (dna) {
+
+    this.in.love  = this.in.love||{};
+
+    let king  = this;
+    let name  = dna.name;
+    let queen = new AppQueen(name, dna);
+
+    king.in.love[name] = queen.love(king);
+
     return this;
 
 }
+function lordNameOfKing (king) {
+    console.log('app',app.in.theNameOf);
+    console.log('i',this);
+    this.in.theNameOf = king;
+    console.log('i',this);
+    console.log('app',app.in.theNameOf);
+    this.do = king.do;
+    return this;
+}
+
+
 function lordDelegator (order) {
 
     let it = order.do||'report';
@@ -55,38 +75,78 @@ function lordDelegator (order) {
     return this;
 
 }
-function lordTakeTheQueen (dna) {
-    // update command list
+function lordWelcomeKing () {
 
-    let queen = new AppQueen(dna);
-    let name  = queen.name;
-    this.love = Object.assign( this.love||{} , {[name]:queen} );
-    return this;
-
-}
-function lordWelcome () {
     this.take( new QueenReporter() );
-    this.do = this.doit;
+    this.do = this.love;
     for ( let everything in this.mind ) this.do(this.mind[everything]);
     return this;
 
 }
 
 
+function QueenReporter (dna) {
 
-function QueenReporter () {
-    this.name = 'report';
-    this.do   = lordDevConsole;
+           // ʕ⊙ᴥ⊙ʔ
+         dna = dna||DNA;
+        for(favor in dna)
+    this[favor] = dna[favor];
+
+    this.name    = 'report';
+    this.f       = lordDevConsole;
+    this.Profile = lordDevProfile;
 }
 function lordDevConsole (rna) {
 
-    console.log(this.name+' ::', rna);
+    if (rna) console.log(this.name+' ::', rna);
+    else this.Profile( this.in.theNameOf || this );
+    
     return this;
 
 }
+function lordDevProfile (noble) {
+    console.group('Profile of ' + noble.name);
+    console.log({
+        name: noble.name,
+        inTheNameOf: noble.in.theNameOf
+    });
+    console.log(noble.in.mind);
+    console.log(noble.in.love);
+    console.groupEnd();
+    return this;
+}
+
 
 
 function QueenMutate () {
     this.name = 'mutate';
     this.do   = lordDevConsole;
 }
+function lordMutateRibosome (rna) {
+
+    let rna_structure = {
+        when:        'classname',
+        has:         'event',
+        do:          'mutate',
+        addClass:    'classname',
+        removeClass: 'classname',
+        toggleClass: 'classname',
+        for:         'classname',
+        wait:        0, // ms
+        than:        (rna)=>false
+    }
+
+    let protein = {
+        type: rna.type ||'mutate',
+        el:   rna.el   || false,
+        tel:  rna.tel  || false
+    };
+
+    // find el
+    if (!protein.el) {
+        //let find_el = rna.forel
+    }
+
+}
+
+//app.do({when:'trigger',has:'click',do:'mutate',addClass:'nigger'});
