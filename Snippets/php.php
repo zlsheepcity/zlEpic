c8b180c04f3efd1ab58f8c91187129b457c6db44
Zinovij Lipinski <zinovi@egamings.com>
2019-06-23 17:25:38 +0300

    wp snippets

--- Snippets/php.php
+++ Snippets/php.php
@@ -45,6 +45,4 @@ $DB_charset = mysql_query("SET NAMES 'utf8'");
 <?php
 // Use shortcodes in form like Landing Page Template.
 echo do_shortcode( '[contact-form-7 id="91" title="quote"]' );
-
  ?>
- 
