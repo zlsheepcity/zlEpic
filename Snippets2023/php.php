4c0ebc9f8d4b84f07cf3b9abf1584e902732d4ed
zlsheepcity <zl.sheepcity@gmail.com>
2023-07-30 19:48:48 +0300

    php server

---
+++ Snippets2023/php.php
@@ -0,0 +1,49 @@
+<?php
+    $xxx = 'xxx';
+?>
+<?= $xxx; ?>
+
+<?php
+
+    $v = $_GET['v'];
+
+?>
+
+<?php
+
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+}
+function endsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+
+    return $length === 0 ||
+    (substr($haystack, -$length) === $needle);
+}
+
+?>
+
+<?php if ($insert_lead): ?>
+<?php echo $insert_lead; ?>
+<?php endif; ?>
+
+
+// --------------------------------- DATABASE
+
+<?php
+
+// SETUP DB
+$mysql_db_host          = 'localhost';
+$mysql_db_login         = 'root';
+$mysql_db_password      = '';
+$mysql_db_databasename  = 'int_rim';
+// CONNECT DB
+$DB_connection_id = mysql_connect($mysql_db_host, $mysql_db_login, $mysql_db_password) or die(mysql_error());
+$DB_select_db = mysql_select_db($mysql_db_databasename) or die(mysql_error());
+$DB_charset = mysql_query("SET NAMES 'utf8'");
+
+?>
+
